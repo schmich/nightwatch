7a743f07ff514b039d6f2a8160edf9f37bdcdbad
Chris Schmich <schmch@gmail.com>
2015-01-01 12:03:22 -0600

    Make Rake hooks more modular for simple removal.

--- lib/nightwatch/middleware/rake.rb
+++ lib/nightwatch/middleware/rake.rb
@@ -1,8 +1,10 @@
 module Nightwatch
   class Rake
     def initialize
-      # TODO: Do this in such a way that is undoable.
-      require 'nightwatch/middleware/rake_methods'
+      @orig_top_level_method = nil
+      @orig_execute_method = nil
+
+      add_hooks
     end
 
     def exception(exception, record)
@@ -10,6 +12,56 @@ module Nightwatch
         nil
       else
         return exception, record
+      end
+    end
+
+    private
+
+    def add_hooks
+      @orig_top_level_method = ::Rake::Application.class_eval do
+        top_level_method = instance_method(:top_level)
+
+        define_method :top_level do |*args, &block|
+          begin
+            top_level_method.bind(self).call(*args, &block)
+          ensure
+            if $!
+              Nightwatch::Monitor.instance.add_exception($!)
+            end
+          end
+        end
+
+        top_level_method
+      end
+
+      @orig_execute_method = ::Rake::Task.class_eval do
+        execute_method = instance_method(:execute)
+
+        define_method :execute do |*args, &block|
+          begin
+            execute_method.bind(self).call(*args, &block)
+          ensure
+            if $!
+              record = { ruby: { rake: { task: self.name } } }
+              Nightwatch::Monitor.instance.add_exception($!, record)
+            end
+          end
+        end
+
+        execute_method
+      end
+    end
+
+    def remove_hooks
+      orig_top_level_method = @orig_top_level_method
+      orig_execute_method = @orig_execute_method
+
+      ::Rake::Application.class_eval do
+        define_method :top_level, orig_top_level_method
+      end
+
+      ::Rake::Task.class_eval do
+        define_method :execute, orig_execute_method
       end
     end
   end
