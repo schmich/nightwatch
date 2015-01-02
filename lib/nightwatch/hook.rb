63cfac5d95e1f2c65299a5807f69f05153c4a80e
Chris Schmich <schmch@gmail.com>
2015-01-02 07:01:58 -0600

    Add Hook#apply to enable enabling/disabling a hook.

--- lib/nightwatch/hook.rb
+++ lib/nightwatch/hook.rb
@@ -3,13 +3,25 @@ module Nightwatch
     def initialize(klass, method, &impl)
       @klass = klass
       @method = method
-      
-      @orig_impl = klass.class_eval do
+
+      @new_impl = impl
+      @orig_impl = nil
+
+      apply
+    end
+
+    def apply
+      return if @orig_impl
+
+      method = @method
+      new_impl = @new_impl
+
+      @orig_impl = @klass.class_eval do
         orig_impl = instance_method(method)
           
         define_method method do |*args, &block|
           bound_method = orig_impl.bind(self)
-          self.instance_exec(bound_method, *args, block, &impl)
+          self.instance_exec(bound_method, *args, block, &new_impl)
         end
 
         orig_impl
@@ -17,12 +29,16 @@ module Nightwatch
     end
 
     def remove
+      return if !@orig_impl
+
       method = @method
       orig_impl = @orig_impl
 
       @klass.class_eval do
         define_method method, orig_impl
       end
+
+      @orig_impl = nil
     end
   end
 end
