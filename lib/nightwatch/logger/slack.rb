b9df557ae7911766c68d9039d71e7ae753bebee4
Chris Schmich <schmch@gmail.com>
2015-01-03 05:59:03 -0600

    Use built-in Net::HTTP over Faraday. Add short and long Slack formatting.

--- lib/nightwatch/logger/slack.rb
+++ lib/nightwatch/logger/slack.rb
@@ -1,10 +1,16 @@
-require 'faraday'
+require 'uri'
+require 'net/http'
 require 'json'
 
 module Nightwatch
   class SlackLogger
     def initialize(opts = {})
       @url = opts[:url]
+      @format = opts[:format] || :long
+
+      if ![:short, :long].include?(@format)
+        raise Error, ":format must be either :short or :long."
+      end
 
       if !@url
         raise Error, 'Webhook URL is required.'
@@ -12,6 +18,32 @@ module Nightwatch
     end
 
     def log(records)
+      texts = []
+
+      if @format == :short
+        method = :short_format
+      else
+        method = :long_format
+      end
+
+      uri = URI(@url)
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = (uri.scheme == 'https')
+
+      req = Net::HTTP::Post.new(uri)
+      req.content_type = 'application/json'
+
+      self.send(method, records) do |payload|
+        req.body = JSON.dump(payload)
+        resp = http.request(req)
+        puts resp
+      end
+    end
+
+    private
+
+    def short_format(records)
       texts = []
 
       records.each do |record|
@@ -26,12 +58,62 @@ module Nightwatch
           text += " at `#{top[:label]}` in #{top[:path]}:#{top[:line]}"
         end
 
+        text += " on #{record[:host][:name]}"
+
         texts << text
       end
 
-      Faraday.new(:url => @url).post do |req|
-        req.headers['Content-Type'] = 'application/json'
-        req.body = JSON.dump({ username: 'Nightwatch', text: texts.join("\n") })
+      payload = {
+        username: 'Nightwatch',
+        text: texts.join("\n")
+      }
+
+      yield payload
+    end
+
+    def long_format(records)
+      records.each do |record|
+        exception = record[:exception]
+        klass = exception[:class]
+        message = exception[:message]
+
+        stack = exception[:stack]
+        stack_value = stack.map do |frame|
+          "`#{frame[:label]}` in #{frame[:path]}:#{frame[:line]}"
+        end.join("\n")
+
+        script = "#{record[:process][:script]} #{record[:process][:argv].join(' ')}",
+        time = Time.at(record[:timestamp]).strftime('%a, %d %b %Y, %T %z')
+        pretext = "`#{klass}`: #{message}"
+
+        payload = {
+          username: 'Nightwatch',
+          attachments: [{
+            mrkdwn_in: ['pretext', 'fields', 'fallback'],
+            text: '',
+            fallback: pretext,
+            pretext: pretext,
+            fields: [{
+              title: 'Stack',
+              value: stack_value,
+              short: true
+            }, {
+              title: 'Host',
+              value: record[:host][:name],
+              short: true
+            }, {
+              title: 'Script',
+              value: script,
+              short: true
+            }, {
+              title: 'Time',
+              value: time,
+              short: true
+            }]
+          }]
+        }
+
+        yield payload
       end
     end
   end
